de3ddbc1a60f901ec29a5d41ddd7d6a729964070
absormu <absoralvord07@gmail.com>
2021-10-27 08:29:19 +0700

    membuat app say-hello

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/absormu/app-say-hello
+
+go 1.17
+
+require github.com/absormu/go-say-hello/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/absormu/go-say-hello/v2"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello("Absor Muhamad"))
+}
